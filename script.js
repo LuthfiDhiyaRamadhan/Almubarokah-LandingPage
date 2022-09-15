3c7b7fed9408fdc60a66043f8d511cff653aff68
Luthfi Dhiya Ramadhan <luthfidhiyar16@gmail.com>
2022-09-16 00:50:55 +0700

    update kegiatan

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+
+
+
+document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
